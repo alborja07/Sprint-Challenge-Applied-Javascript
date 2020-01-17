dacbf939c3118b52d3621282e941470b0bb21c7d
alexis villaraza <alexis.borja@gmail.com>
2020-01-17 09:57:14 -0700

    added content in tabs

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,46 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+function topics (obj){
+    const tab = document.createElement('div');
+    const topics = document.createElement('ul');
+    const topic1 = document.createElement('li');
+    const topic2 = document.createElement('li');
+    const topic3 = document.createElement('li');
+    const topic4 = document.createElement('li');
+    const topic5 = document.createElement('li');
+
+    tab.append(topics);
+    topics.append(topic1);
+    topics.append(topic2);
+    topics.append(topic3);
+    topics.append(topic4);
+    topics.append(topic5);
+
+    tab.classList.add('tabs');
+    topics.classList.add('topics');
+    topic1.classList.add(tab);
+    topic2.classList.add(tab);
+    topic3.classList.add(tab);
+    topic4.classList.add(tab);
+    topic5.classList.add(tab);
+
+    topics.textContent = "Topics";
+    topic1.textContent = topics[0];
+    topic2.textContent = topics[0];
+    topic1.textContent = topics[0];
+    topic1.textContent = topics[0];
+    topic1.textContent = topics[0]; 
+
+    return tab;
+}
+
+tabList = document.querySelector('.title');
+
+for (var i = 0; i < topics.length; i++){
+    axios.get('https://lambda-times-backend.herokuapp.com/topics' + topics[i])
+    .then(response => {
+        console.log(response);
+    })
+}
